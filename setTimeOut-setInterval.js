be1e99203c14c752116878942e929f6b77ec597e
abrahamMartinez <bmtzs@me.com>
2023-07-18 15:37:41 +0200

    sets

--- setTimeOut-setInterval.js
+++ setTimeOut-setInterval.js
@@ -1,7 +1,16 @@
-console.log("Hello")
 
+console.log("Hello")
+//Will delay the execution of the function for a given time
 setTimeout(() => {
     console.log("...late")
-}, 3000)
+}, 3000) //Miliseconds
 
 console.log("Goodbye")
+
+// Interval: Will execute the inner function at given interval
+
+const id = setInterval(() =>{
+    console.log(Math.random())
+}, 2000)
+// We need an stoping
+clearInterval(id)   
