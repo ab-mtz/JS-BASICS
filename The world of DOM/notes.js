8bd5b26c9aa629b0c56ab2d50be1806be6b290ce
abrahamMartinez <bmtzs@me.com>
2023-07-20 11:48:54 +0200

    Dom

---
+++ The world of DOM/notes.js
@@ -0,0 +1,19 @@
+// DOM: Document Object Model
+    // Is a JS representation of a webpage.
+    // The JS 'window' into the contents of a webpage
+    // Bunch of objects that you can interact with via JS
+
+
+// SELECT VS MANIPULATE
+
+    // Select
+    document.getElementById
+
+    
+
+    
+    document.getElementsByTagName
+    
+    
+    
+    document.getElementsByClassName
