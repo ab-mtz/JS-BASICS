0cdce720def7e4624af3a7de8f565540bd98fb02
abrahamMartinez <bmtzs@me.com>
2023-07-24 18:41:55 +0200

    onclick, addeventlistener

--- The world of DOM/events.js
+++ The world of DOM/events.js
@@ -2,4 +2,25 @@
 
 // clicks
 
-const onClick = 
+const btn = document.querySelector('#v2');
+
+btn.onclick = function () {
+    console.log('you clicked me')
+}
+
+function scream (){
+    console.log ('scream')
+}
+
+btn.onmouseenter = scream;
+
+//Add event listener
+
+const btn2 = document.querySelector('#btn2');
+
+btn2.addEventListener('click', () => {
+    console.log('buton 2')
+}) //Allows to call two different functions simultaneusly
+// The go to 
+
+
