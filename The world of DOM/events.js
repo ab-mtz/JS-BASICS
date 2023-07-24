2ee967588c3e3b01c9fd9d45b927d2107bb6c834
abrahamMartinez <bmtzs@me.com>
2023-07-24 18:15:59 +0200

    events

---
+++ The world of DOM/events.js
@@ -0,0 +1,5 @@
+// clicks, drag, frop, hover, scrill, form submission, key press, focus/blur, mouse wheel, double click, copying, pasting, audo start, screen resize, printing
+
+// clicks
+
+const onClick = 
