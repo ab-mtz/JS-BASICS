663e4d4c1c9ce9e50207dda02bce5eb0ecd596bd
abrahamMartinez <bmtzs@me.com>
2023-07-26 15:46:38 +0200

    form events, prevent default

--- The world of DOM/events.js
+++ The world of DOM/events.js
@@ -40,8 +40,53 @@ input.addEventListener('keydown', function (event) {
 // })
 
 //For exemple to a game where you need the event listener not for an input
+//Event handler
 
 window.addEventListener('keydown', function (event) {
-    console.log(event.key)
-    console.log('Move down')
+    switch (event.code) {
+        case 'ArrowUp':
+            console.log("Move up!");
+            break;
+        case 'ArrowDown':
+            console.log("Move down!");
+            break;
+        case 'ArrowLeft':
+            console.log("Move left!");
+            break;
+        case 'ArrowRight':
+            console.log("Move Right!");
+            break;
+        default:
+            console.log("Ignored");
+    }
+})
+
+//FORM EVENTS & PREVENT DEFAULT 
+
+const form = document.querySelector('#shelterForm')
+form.addEventListener("submit", function (event) {
+    console.log("submited")
+})
+//In This way, the default beheivour of forms is to refresh the page
+//In order to avoid that we use:
+
+const form = document.querySelector('#shelterForm')
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("submited");
+})
+//We can use for a single page app
+
+//Example storing values into a list from input field preventing refresh of form
+const form = document.querySelector('#shelterForm');
+const input = document.querySelector("catName");
+const list = document.querySelector("#list");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(input.value);
+    const catName  = input.value;
+    const newLi = document.createElement("LI");
+    newLi.innerText(newLi);
+    list.append(newLi);
+
 })
